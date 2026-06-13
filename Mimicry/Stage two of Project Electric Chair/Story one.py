db0fd0d3fb372f3f6b1a5670199ac9f1edf79baa
fe312842 <fe312842@falmouth.ac.uk>
2026-06-13 17:29:29 +0100

    Moved files around and working on stage two

---
+++ Mimicry/Stage two of Project Electric Chair/Story one.py
@@ -0,0 +1,26 @@
+def listen():
+    return 
+def speak():
+    return
+def light_on():
+    return
+def light_off():
+    return
+def music_start(): # play certain tracks represents playing good music / comfort and calm
+    return
+def music_off(): # turn off tracks
+    return
+def scene_one(): # key gameplay loop
+    question_one()#do this as self.init
+    response = input()
+    if response == answer():
+        light_on()
+        music_start()
+    else:
+        music_off()
+        light_off()
+    return
+def question_one():
+    return
+def answer(): # do this as self.init
+    return
---
+++ Mimicry/Stage two of Project Electric Chair/readme.md
@@ -0,0 +1,4 @@
+# Electric chair game
+I had this idea over a year ago for a first year project but lacked the skills to complete it or the time. While I don't have the skills I can at least adjust the ceiling to make something.
+
+The idea is as follows
