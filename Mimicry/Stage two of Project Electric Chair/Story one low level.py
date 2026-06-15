29e00c2e2c974dd6351561ebf75e54bc06dd6d6f
fe312842 <fe312842@falmouth.ac.uk>
2026-06-15 17:22:00 +0100

    Starting to make stage two prototype from page code

---
+++ Mimicry/Stage two of Project Electric Chair/Story one low level.py
@@ -0,0 +1,15 @@
+import speech_recognition as sr
+
+class QUESTIONING:
+    def __init__(self):
+        self.Question_one = "Where were you at during this time"
+        self.answer_one = []
+        self.Question_two = "What were you doing"
+        self.answer_two = []
+        self.Question_three = "Who is this person"
+        self.answer_three = []
+        self.Question_four = "How do you know ___"
+        self.answer_four = []
+        self.Question_five = "Why  did you do this?"
+        self.answer_five = []
+array = QUESTIONING()
