ced7d5d5d484fbfea500e4d8cf976056b27d61f0
fe312842 <fe312842@falmouth.ac.uk>
2026-06-16 10:36:21 +0100

    Added low level baseline version of story one for stage two

--- Mimicry/Stage two of Project Electric Chair/Story one low level.py
+++ Mimicry/Stage two of Project Electric Chair/Story one low level.py
@@ -1,4 +1,54 @@
 import speech_recognition as sr
+import time
+import pyttsx3
+
+def listen():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+
+        print("Listening...")
+
+        recognizer.adjust_for_ambient_noise(source, duration=1)
+
+        audio = recognizer.listen(
+            source,
+            timeout=5,
+            phrase_time_limit=5
+        )
+
+        print("Finished listening")
+
+    try:
+
+        print("Recognizing...")
+
+        text = recognizer.recognize_google(audio)
+
+        print("You said:", text)
+
+        return text.lower()
+
+    except Exception as e:
+
+        print("Error:", e)
+
+        return ""
+
+
+def speak(text):
+    engine = pyttsx3.init()
+
+    print(text)
+
+    engine.say(text)
+
+    engine.runAndWait()
+
+    engine.stop()
+
+    time.sleep(0.5)
+
 
 class QUESTIONING:
     def __init__(self):
@@ -12,4 +62,63 @@ class QUESTIONING:
         self.answer_four = []
         self.Question_five = "Why  did you do this?"
         self.answer_five = []
+
+
 array = QUESTIONING()
+total_score = 0
+
+
+def stage_one():
+    speak(array.Question_one)
+    array.answer_one.append(listen())
+    speak(array.Question_two)
+    array.answer_two.append(listen())
+    speak(array.Question_three)
+    array.answer_three.append(listen())
+    speak(array.Question_four)
+    array.answer_four.append(listen())
+    speak(array.Question_five)
+    array.answer_five.append(listen())
+    speak("Thank you for your time, we'll be in touch")
+
+
+def stage_two():
+    score = 0
+    speak("Please answer the following questions")
+    speak(array.Question_one)
+    response_one = listen()
+    if response_one in array.answer_one:
+        score += 1
+    else:
+        score += 0
+    speak(array.Question_two)
+    response_two = listen()
+    if response_two in array.answer_two:
+        score += 1
+    else:
+        score += 0
+    speak(array.Question_three)
+    response_three = listen()
+    if response_three in array.answer_three:
+        score += 1
+    else:
+        score += 0
+    speak(array.Question_four)
+    response_four = listen()
+    if response_four in array.answer_four:
+        score += 1
+    else:
+        score += 0
+    speak(array.Question_five)
+    response_five = listen()
+    if response_five in array.answer_five:
+        score += 1
+    else:
+        score += 0
+    return score
+
+
+def main():
+    stage_one()
+    total_score = stage_two()
+    speak(f"Your total score is {total_score} out of 5")
