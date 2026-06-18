1cf6c6383e6ec8bfccc966a79f197229f341f91c
fe312842 <fe312842@falmouth.ac.uk>
2026-06-18 11:52:43 +0100

    Completed the system architecture / skeleton of the second stage

--- Mimicry/Stage two of Project Electric Chair/Story one low level.py
+++ Mimicry/Stage two of Project Electric Chair/Story one low level.py
@@ -2,6 +2,7 @@ import speech_recognition as sr
 import time
 import pyttsx3
 
+
 def listen():
     recognizer = sr.Recognizer()
 
@@ -37,6 +38,7 @@ def listen():
 
 
 def speak(text):
+
     engine = pyttsx3.init()
 
     print(text)
@@ -52,73 +54,114 @@ def speak(text):
 
 class QUESTIONING:
     def __init__(self):
-        self.Question_one = "Where were you at during this time"
+        self.Question_one = "Where were you during this time?"
         self.answer_one = []
-        self.Question_two = "What were you doing"
+
+        self.Question_two = "What were you doing?"
         self.answer_two = []
-        self.Question_three = "Who is this person"
+
+        self.Question_three = "Who is this person?"
         self.answer_three = []
-        self.Question_four = "How do you know ___"
+
+        self.Question_four = "How do you know ___?"
         self.answer_four = []
-        self.Question_five = "Why  did you do this?"
+
+        self.Question_five = "Why did you do this?"
         self.answer_five = []
+
+        self.insults = [
+            "asshole", "idiot", "moron", "stupid", "dumb",
+            "fool", "jerk", "loser", "twit", "nincompoop",
+            "imbecile", "cretin", "blockhead", "dunce",
+            "nitwit", "simpleton", "clown", "buffoon",
+            "dolt", "dimwit", "halfwit", "ignoramus",
+            "ninny", "numskull", "pinhead",
+            "scatterbrain", "airhead"
+        ]
 
 
 array = QUESTIONING()
-total_score = 0
 
 
 def stage_one():
+
     speak(array.Question_one)
     array.answer_one.append(listen())
+
     speak(array.Question_two)
     array.answer_two.append(listen())
+
     speak(array.Question_three)
     array.answer_three.append(listen())
+
     speak(array.Question_four)
     array.answer_four.append(listen())
+
     speak(array.Question_five)
     array.answer_five.append(listen())
-    speak("Thank you for your time, we'll be in touch")
+
+    speak("Thank you for your time, we'll be in touch.")
 
 
 def stage_two():
+
     score = 0
-    speak("Please answer the following questions")
+
+    speak("Please answer the following questions.")
+
     speak(array.Question_one)
     response_one = listen()
-    if response_one in array.answer_one:
+
+    if any(answer in response_one for answer in array.answer_one):
         score += 1
-    else:
-        score += 0
+
     speak(array.Question_two)
     response_two = listen()
-    if response_two in array.answer_two:
+
+    if any(answer in response_two for answer in array.answer_two):
         score += 1
-    else:
-        score += 0
+
     speak(array.Question_three)
     response_three = listen()
-    if response_three in array.answer_three:
+
+    if any(answer in response_three for answer in array.answer_three):
         score += 1
-    else:
-        score += 0
+
     speak(array.Question_four)
     response_four = listen()
-    if response_four in array.answer_four:
+
+    if any(answer in response_four for answer in array.answer_four):
         score += 1
-    else:
-        score += 0
+
     speak(array.Question_five)
     response_five = listen()
-    if response_five in array.answer_five:
-        score += 1
-    else:
-        score += 0
-    return score
 
+    if any(answer in response_five for answer in array.answer_five):
+        score += 1
+
+    if score > 2:
+
+        speak("Wow, this guy needs to be taken down a peg.")
+
+        insult = listen()
+
+        if any(word in insult for word in array.insults):
+            score += 10
+        else:
+            score -= 10
+
+    return score
+def stage_three(total_score):
+    if total_score > 15:
+        speak("You have been found innocent")
+    else:
+        speak("You have been found guilty")
 
 def main():
     stage_one()
     total_score = stage_two()
-    speak(f"Your total score is {total_score} out of 5")
+    stage_three(total_score)
+
+
+if __name__ == "__main__":
+    main()
